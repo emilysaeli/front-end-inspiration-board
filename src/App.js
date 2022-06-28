ac95e16ec1ea1b02c5114407976cac4254839f69
Gabriela Webb <gabw813@gmail.com>
2022-06-28 11:16:07 -0700

    adds more Board and Card scaffolding

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Board from "./Board";
 import Card from "./Card";
 
 function App() {
+  const [cards, setCards] = useState([
+    {
+      id: 0,
+      title: "card #1",
+      details: "this is a test card!",
+    },
+  ]);
   return (
-    <div>
-      <header></header>
+    <main>
+      <h1>Team Sunshine Inspiration Board</h1>
       <Board></Board>
       <Card></Card>
-    </div>
+    </main>
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -1,3 +1,21 @@
-const Card = () => {};
+// requirements:
+// Create a new card for the selected board by filling out a form
+// Display an error message if invalid or incomplete information is provided
+// Display all cards of a selected board
+// Delete a card
+// “+1” a card
+
+const Card = () => {
+  return (
+    <div>
+      <h2>Card</h2>
+      <ul>
+        <li>id: 1</li>
+        <li>message: hello!</li>
+        <li>likes: 1</li>
+      </ul>
+    </div>
+  );
+};
 
 export default Card;
